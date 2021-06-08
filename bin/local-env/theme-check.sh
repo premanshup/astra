d3a224d5750eaa1fe4a69884926313a32f412811
premanshup <premanshup@bsf.io>
2021-06-08 17:58:27 +0530

    Updated the theme check WP CLI commands

--- bin/local-env/theme-check.sh
+++ bin/local-env/theme-check.sh
@@ -1,3 +1,5 @@
+wp plugin install theme-check
+wp plugin activate theme-check
 wp package install anhskohbo/wp-cli-themecheck
-wp plugin install theme-check --activate --allow-root
-wp themecheck --theme=astra --no-interactive --allow-root
+wp plugin install theme-check --activate
+wp themecheck --theme=astra --no-interactive
