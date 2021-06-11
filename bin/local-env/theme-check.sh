a4c110afb035d349d52c553443a973c027d1869e
premanshup <premanshup@bsf.io>
2021-06-11 23:01:19 +0530

    Docker WP CLI.

--- bin/local-env/theme-check.sh
+++ bin/local-env/theme-check.sh
@@ -1,5 +1,4 @@
 wp plugin install theme-check
 wp plugin activate theme-check
-wp package install anhskohbo/wp-cli-themecheck
-wp plugin install theme-check --activate
+php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck --allow-root
 wp themecheck --theme=astra --no-interactive
