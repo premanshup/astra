c7c1376ddf6f05f6a58f15b3650c6334fc5388ef
premanshup <premanshup@bsf.io>
2021-06-12 14:13:55 +0530

    Update version

--- bin/install-theme-check.sh
+++ bin/install-theme-check.sh
@@ -1,5 +1,11 @@
+CLI='cli'
+CONTAINER='wordpress'
+DATABASE='mysql'
+SITE_TITLE='Astra Dev'
+$HOST_PORT=$(dc port wordpress 80 | awk -F : '{printf $2}')
+
 php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck --allow-root
 echo "Installing WP"
-wp  --allow-root core install --url=http://127.0.0.1:8889 --title=SandboxSite --admin_user=admin --admin_password=admin --admin_email=admin@admin.com
+wp core install --title="$SITE_TITLE" --admin_user=admin --admin_password=password --admin_email=test@test.com --skip-email --url=http://localhost:$HOST_PORT
 wp plugin install theme-check --activate --allow-root
 wp themecheck --theme=astra --no-interactive --allow-root
