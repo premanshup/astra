586761d57a23f75c81f9c03246ce8d874d587aa2
premanshup <premanshup@bsf.io>
2021-06-12 14:16:33 +0530

    Allow root

--- bin/install-theme-check.sh
+++ bin/install-theme-check.sh
@@ -6,6 +6,6 @@ $HOST_PORT=$(dc port wordpress 80 | awk -F : '{printf $2}')
 
 php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck --allow-root
 echo "Installing WP"
-wp core install --title="$SITE_TITLE" --admin_user=admin --admin_password=password --admin_email=test@test.com --skip-email --url=http://localhost:$HOST_PORT
+wp core install --title="$SITE_TITLE" --admin_user=admin --admin_password=password --admin_email=test@test.com --skip-email --url=http://localhost:$HOST_PORT --allow-root
 wp plugin install theme-check --activate --allow-root
 wp themecheck --theme=astra --no-interactive --allow-root
