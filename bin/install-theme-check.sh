7405068bdb19079ee76697ff453366cd98ca4ea7
premanshup <premanshup@bsf.io>
2021-06-12 14:03:22 +0530

    Installing wp

--- bin/install-theme-check.sh
+++ bin/install-theme-check.sh
@@ -1,4 +1,5 @@
 php -d memory_limit=1024M "$(which wp)" package install anhskohbo/wp-cli-themecheck --allow-root
+echo "Installing WP"
 wp  --allow-root core install --url=http://localhost:8080 --title=SandboxSite --admin_user=admin --admin_password=admin --admin_email=admin@admin.com
 wp plugin install theme-check --activate --allow-root
-wp themecheck --theme=neve --no-interactive --allow-root
+wp themecheck --theme=astra --no-interactive --allow-root
